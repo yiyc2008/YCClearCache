2376677dbc1446d94971d18c5bb91e21082db280
yiyc2008 <Berton@berton-macbook-pro.local>
2015-12-21 19:30:10 +0800

    Initial Commit

---
+++ YCClearCache/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YCClearCache
+//
+//  Created by Berton on 15/12/21.
+//  Copyright © 2015年 Berton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
